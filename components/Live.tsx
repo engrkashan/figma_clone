24422646bddca2f32ad6ca2c597df8ce20c7affd
engrkashan <kshfkhl@gmail.com>
2024-02-05 00:46:08 +0500

    :fire: added config files

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,5 +1,13 @@
+import LiveCursors from "./cursor/LiveCursors";
+import { useOthers } from "@/liveblocks.config";
+
 const Live = () => {
-  return <div>Live</div>;
+  const others = useOthers();
+  return (
+    <div>
+      <LiveCursors others={others} />
+    </div>
+  );
 };
 
 export default Live;
